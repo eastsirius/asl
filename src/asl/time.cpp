f0cbb82e6f8b68ed5a2fb8002165c113f0ff7ba1
cxt <cxt1989422@126.com>
2019-02-17 16:31:47 +0800

    添加日期时间工具

---
+++ src/asl/time.cpp
@@ -0,0 +1,111 @@
+﻿/**
+ * @file time.cpp
+ * @brief 时间工具
+ * @author 程行通
+ */
+
+#include "time.hpp"
+#ifdef WINDOWS
+#  include <windows.h>
+#else
+#  include <sys/time.h>
+#endif
+
+namespace ASL_NAMESPACE {
+	Time::Time() : m_s64MicrosecTime(0) {
+	}
+
+	Time::Time(int64_t value) : m_s64MicrosecTime(value) {
+	}
+
+	Time Time::GetTime() {
+#ifdef WINDOWS
+		SYSTEMTIME st;
+		::GetLocalTime(&st);
+		FILETIME ft;
+		::SystemTimeToFileTime(&st, &ft);
+		return Time(((((uint64_t)ft.dwHighDateTime) << 32) + ft.dwLowDateTime) / 10);
+#else
+		timeval t;
+		gettimeofday(&t, 0);
+		return Time(t.tv_sec * 1000000ULL + t.tv_usec);
+#endif
+	}
+
+
+	Datetime::Datetime() {
+		m_nYear = 0;
+		m_nMonth = 0;
+		m_nDayOfWeek = 0;
+		m_nDay = 0;
+		m_nHour = 0;
+		m_nMinute = 0;
+		m_nSecond = 0;
+		m_nMilliseconds = 0;
+		m_nMicroseconds = 0;
+	}
+
+	Datetime Datetime::GetSystemTime() {
+		return ToSystemTime(Time::GetTime());
+	}
+
+	Datetime Datetime::GetLocalTime() {
+		return ToLocalTime(Time::GetTime());
+	}
+
+	Datetime Datetime::ToSystemTime(Time value) {
+		time_t tv = (time_t)(value.Value() / 1000000);
+		Datetime dt;
+		dt.setDatetime(value, gmtime(&tv));
+		return dt;
+	}
+
+	Datetime Datetime::ToLocalTime(Time value) {
+		time_t tv = (time_t)(value.Value() / 1000000);
+		Datetime dt;
+		dt.setDatetime(value, localtime(&tv));
+		return dt;
+	}
+
+	void Datetime::setDatetime(Time value, tm* dt) {
+		m_nYear = dt->tm_year + 1900;
+		m_nMonth = dt->tm_mon + 1;
+		m_nDayOfWeek = dt->tm_wday + 1;
+		m_nDay = dt->tm_mday;
+		m_nHour = dt->tm_hour;
+		m_nMinute = dt->tm_min;
+		m_nSecond = dt->tm_sec;
+		m_nMilliseconds = (int)(value.Value() / 1000 % 1000);
+		m_nMicroseconds = (int)(value.Value() % 1000);
+	}
+
+
+	Timer::Timer() {
+		Restart();
+	}
+
+	void Timer::Restart() {
+		m_tBeginTime = Time::GetTime();
+	}
+
+	int64_t Timer::SecTime() const {
+		return MicrosecTime() / 1000000;
+	}
+
+	int64_t Timer::MillisecTime() const {
+		return MicrosecTime() / 1000;
+	}
+
+	int64_t Timer::MicrosecTime() const {
+		return Time::GetTime().Value() - m_tBeginTime.Value();
+	}
+
+
+	int64_t asl_get_ms_time() {
+		return asl_get_us_time() / 1000;
+	}
+
+	int64_t asl_get_us_time() {
+		return Time::GetTime().Value();
+	}
+}
---
+++ src/asl/time.hpp
@@ -0,0 +1,218 @@
+﻿/**
+ * @file time.hpp
+ * @brief 时间工具
+ * @author 程行通
+ */
+
+#pragma once
+
+#include "asldef.hpp"
+#include <stdint.h>
+#include <time.h>
+
+namespace ASL_NAMESPACE {
+	/**
+	 * @class Time
+	 * @brief 时间类
+	 */
+	class Time {
+	public:
+		Time();
+		Time(int64_t value);
+
+	public:
+		/**
+		 * @brief 获取微秒时间值
+		 * @return 返回微秒时间值
+		 */
+		int64_t Value() const {
+			return m_s64MicrosecTime;
+		}
+
+	public:
+		/**
+		 * @brief 获取微秒时间
+		 * @return 返回微秒时间
+		 */
+		static Time GetTime();
+
+	private:
+		int64_t m_s64MicrosecTime;
+	};
+
+	/**
+	 * @class Datetime
+	 * @brief 日期时间
+	 */
+	class Datetime {
+	public:
+		Datetime();
+
+	public:
+		/**
+		 * @brief 获取年份
+		 * @return 返回年份
+		 */
+		int GetYear() const {
+			return m_nYear;
+		}
+
+		/**
+		 * @brief 获取月份
+		 * @return 返回月份
+		 */
+		int GetMonth() const {
+			return m_nMonth;
+		}
+
+		/**
+		 * @brief 获取日期
+		 * @return 返回日期
+		 */
+		int GetDay() const {
+			return m_nDay;
+		}
+
+		/**
+		 * @brief 获取周几
+		 * @return 返回周几
+		 */
+		int GetDayOfWeek() const {
+			return m_nDayOfWeek;
+		}
+
+		/**
+		 * @brief 获取小时数
+		 * @return 返回小时数
+		 */
+		int GetHour() const {
+			return m_nHour;
+		}
+
+		/**
+		 * @brief 获取分钟数
+		 * @return 返回分钟数
+		 */
+		int GetMinute() const {
+			return m_nMinute;
+		}
+
+		/**
+		 * @brief 获取秒数
+		 * @return 返回秒数
+		 */
+		int GetSecond() const {
+			return m_nSecond;
+		}
+
+		/**
+		 * @brief 获取毫秒数
+		 * @return 返回微秒数
+		 */
+		int GetMillisecond() const {
+			return m_nMilliseconds;
+		}
+
+		/**
+		 * @brief 获取微秒数
+		 * @return 返回微秒数
+		 */
+		int GetMicrosecond() const {
+			return m_nMicroseconds;
+		}
+
+	public:
+		/**
+		 * @brief 获取系统时间(格林时间)
+		 * @return 返回系统时间
+		 */
+		static Datetime GetSystemTime();
+
+		/**
+		 * @brief 获取本地时间
+		 * @return 返回本地时间
+		 */
+		static Datetime GetLocalTime();
+
+		/**
+		 * @brief 转换到系统时间(格林时间)
+		 * @return 返回系统时间
+		 */
+		static Datetime ToSystemTime(Time value);
+
+		/**
+		 * @brief 转换到本地时间
+		 * @return 返回本地时间
+		 */
+		static Datetime ToLocalTime(Time value);
+
+	private:
+		/**
+		 * @brief 设置日期时间
+		 * @param value 时间值
+		 * @param dt 日期
+		 */
+		void setDatetime(Time value, tm* dt);
+
+	private:
+		Time m_tValue;
+		int m_nYear;
+		int m_nMonth;
+		int m_nDayOfWeek;
+		int m_nDay;
+		int m_nHour;
+		int m_nMinute;
+		int m_nSecond;
+		int m_nMilliseconds;
+		int m_nMicroseconds;
+	};
+
+	/**
+	 * @class Timer
+	 * @brief 计时器
+	 */
+	class Timer
+	{
+	public:
+		Timer();
+
+	public:
+		/**
+		 * @brief 重启计时器
+		 */
+		void Restart();
+
+		/**
+		 * @brief 获取当前时间(s)
+		 * @return 返回当前时间
+		 */
+		int64_t SecTime() const;
+
+		/**
+		 * @brief 获取当前时间(ms)
+		 * @return 返回当前时间
+		 */
+		int64_t MillisecTime() const;
+
+		/**
+		 * @brief 获取当前时间(us)
+		 * @return 返回当前时间
+		 */
+		int64_t MicrosecTime() const;
+
+	private:
+		Time m_tBeginTime;
+	};
+
+	/**
+	 * @class 获取毫秒时间值
+	 * @brief 返回毫秒时间值
+	 */
+	int64_t asl_get_ms_time();
+
+	/**
+	 * @class 获取微秒时间值
+	 * @brief 返回微秒时间值
+	 */
+	int64_t asl_get_us_time();
+}
