c326e3c4564b486d787a92053f9d2372fa889175
cxt <cxt1989422@126.com>
2019-02-18 14:51:00 +0800

    添加参数解析器

---
+++ src/asl/flags.cpp
@@ -0,0 +1,236 @@
+﻿/**
+ * @file flags.cpp
+ * @brief 程序选项解析
+ * @author 程行通
+ */
+
+#include "flags.hpp"
+#include "utils.hpp"
+#include <cstring>
+
+namespace ASL_NAMESPACE {
+    FlagBase::FlagBase(const char* szKey, const char* szShortKey, const char* szUsage)
+        : m_strKey(szKey), m_strShortKey(szShortKey), m_strUsage(szUsage) {
+    }
+
+    FlagBase::~FlagBase() {
+    }
+
+    void FlagBase::PrintUsage(int nKeyLen) const {
+        std::string strKey = _PrintKey(nKeyLen);
+        printf("    %s  %s\n", strKey.c_str(), GetUsage().c_str());
+    }
+
+    std::string FlagBase::_PrintKey(int nKeyLen) const {
+        std::string ret;
+        if(GetKey().length() > 0) {
+            ret += "--";
+            ret += GetKey();
+            if(GetShortKey().length() > 0) {
+                ret += ",";
+            }
+        }
+        if(GetShortKey().length() > 0) {
+            ret += "-";
+            ret += GetShortKey();
+        }
+
+        size_t cnt = nKeyLen - GetKey().length() - GetShortKey().length();
+        for(size_t i = 0; i < cnt; ++i) {
+            ret += " ";
+        }
+
+        return ret;
+    }
+
+
+    StringFlag::StringFlag(std::string& strValue, const char* szKey, const char* szShortKey,
+            const char* szUsage, std::string strDefaultValue)
+            : FlagBase(szKey, szShortKey, szUsage), m_strValue(strValue) {
+        m_strValue = strDefaultValue;
+    }
+
+    StringFlag::~StringFlag() {
+    }
+
+    bool StringFlag::ParseFlag(const char* szValue) {
+        if(szValue) {
+            m_strValue = szValue;
+        } else {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    IntFlag::IntFlag(int& nValue, const char* szKey, const char* szShortKey,
+            const char* szUsage, int nDefaultValue) : FlagBase(szKey, szShortKey, szUsage), m_nValue(nValue) {
+        m_nValue = nDefaultValue;
+    }
+
+    IntFlag::~IntFlag() {
+    }
+
+    bool IntFlag::ParseFlag(const char* szValue) {
+        if(szValue) {
+            m_nValue = atoi(szValue);
+        } else {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    BoolFlag::BoolFlag(bool& bValue, const char* szKey, const char* szShortKey,
+            const char* szUsage, bool bDefaultValue) : FlagBase(szKey, szShortKey, szUsage), m_bValue(bValue) {
+        m_bValue = bDefaultValue;
+    }
+
+    BoolFlag::~BoolFlag() {
+    }
+
+    bool BoolFlag::ParseFlag(const char* szValue) {
+        if(szValue) {
+            if(strcasecmp(szValue, "true") == 0) {
+                m_bValue = true;
+            } else if(strcasecmp(szValue, "false") == 0) {
+                m_bValue = false;
+            } else if(strcasecmp(szValue, "yes") == 0) {
+                m_bValue = true;
+            } else if(strcasecmp(szValue, "no") == 0) {
+                m_bValue = false;
+            } else if(strcasecmp(szValue, "on") == 0) {
+                m_bValue = true;
+            } else if(strcasecmp(szValue, "off") == 0) {
+                m_bValue = false;
+            } else {
+                return false;
+            }
+        } else {
+            m_bValue = true;
+        }
+
+        return true;
+    }
+
+
+    FloatFlag::FloatFlag(float& fValue, const char* szKey, const char* szShortKey,
+            const char* szUsage, float fDefaultValue) : FlagBase(szKey, szShortKey, szUsage), m_fValue(fValue) {
+        m_fValue = fDefaultValue;
+    }
+
+    FloatFlag::~FloatFlag() {
+    }
+
+    bool FloatFlag::ParseFlag(const char* szValue) {
+        if(szValue) {
+            m_fValue = (float)atof(szValue);
+        } else {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    Flags::Flags() {
+    }
+
+    bool Flags::Parse(int argc, const char** args) {
+        int i = 1;
+        while(i < argc) {
+            if(args[i][0] != '-') {
+                return false;
+            }
+            if(strlen(args[i]) < 2) {
+                return false;
+            }
+
+            FlagPtr_t pValue;
+            if(args[i][1] == '-') {
+                if(strlen(args[i]) < 3) {
+                    return false;
+                }
+
+                auto iter = m_mpKeyValueMap.find(args[i] + 2);
+                if(iter != m_mpKeyValueMap.end()) {
+                    pValue = iter->second;
+                }
+            } else {
+                auto iter = m_mpShortKeyValueMap.find(args[i] + 1);
+                if(iter != m_mpShortKeyValueMap.end()) {
+                    pValue = iter->second;
+                }
+            }
+
+            if(!pValue) {
+                return false;
+            }
+            ++i;
+
+            const char* szValue = NULL;
+            if(i < argc) {
+                if(args[i][0] != '-') {
+                    szValue = args[i];
+                    ++i;
+                }
+            }
+
+            if(!pValue->ParseFlag(szValue)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void Flags::Usage() {
+        int nMaxSize = 0;
+        for(auto iter = m_mpValues.begin(); iter != m_mpValues.end(); ++iter) {
+            int nSize = iter->second->GetKey().length() + iter->second->GetShortKey().length();
+            nMaxSize = asl_max(nSize, nMaxSize);
+        }
+
+        printf("Usage:\n");
+        for(auto iter = m_mpValues.begin(); iter != m_mpValues.end(); ++iter) {
+            iter->second->PrintUsage(nMaxSize);
+        }
+    }
+
+    void Flags::StringValue(std::string& strValue, const char* szKey, const char* szShortKey,
+            const char* szUsage, std::string strDefaultValue) {
+        _AddValue(szKey, szShortKey, new StringFlag(strValue, szKey, szShortKey, szUsage, strDefaultValue));
+    }
+
+    void Flags::IntValue(int& nValue, const char* szKey, const char* szShortKey,
+            const char* szUsage, int nDefaultValue) {
+        _AddValue(szKey, szShortKey, new IntFlag(nValue, szKey, szShortKey, szUsage, nDefaultValue));
+    }
+
+    void Flags::BoolValue(bool& bValue, const char* szKey, const char* szShortKey,
+            const char* szUsage, bool bDefaultValue) {
+        _AddValue(szKey, szShortKey, new BoolFlag(bValue, szKey, szShortKey, szUsage, bDefaultValue));
+    }
+
+    void Flags::FloatValue(float& fValue, const char* szKey, const char* szShortKey,
+            const char* szUsage, float fDefaultValue) {
+        _AddValue(szKey, szShortKey, new FloatFlag(fValue, szKey, szShortKey, szUsage, fDefaultValue));
+    }
+
+    void Flags::_AddValue(const char* szKey, const char* szShortKey, FlagBase* pValue) {
+        FlagPtr_t value(pValue);
+        if(szKey) {
+            m_mpKeyValueMap[szKey] = value;
+        }
+        if(szShortKey) {
+            m_mpShortKeyValueMap[szShortKey] = value;
+        }
+        if(szKey) {
+            m_mpValues[szKey] = value;
+        } else if(szShortKey) {
+            m_mpValues[szShortKey] = value;
+        }
+    }
+}
---
+++ src/asl/flags.hpp
@@ -0,0 +1,238 @@
+﻿/**
+ * @file flags.hpp
+ * @brief 程序选项解析
+ * @author 程行通
+ */
+
+#pragma once
+
+#include "asldef.hpp"
+#include <string>
+#include <memory>
+#include <vector>
+#include <map>
+
+namespace ASL_NAMESPACE {
+	/**
+     * @brief 选项基类
+     */
+	class FlagBase {
+	public:
+		FlagBase(const char* szKey, const char* szShortKey, const char* szUsage);
+		virtual ~FlagBase();
+
+	public:
+		/**
+		 * @brief 解析选项
+		 * @param szValue 选项值
+		 * @return 返回执行结果
+		 */
+		virtual bool ParseFlag(const char* szValue) = 0;
+
+		/**
+		 * @brief 打印用法
+		 * @param nKeyLen 键值最大长度
+		 */
+		void PrintUsage(int nKeyLen) const;
+
+		/**
+		 * @brief 获取键值
+		 */
+		const std::string& GetKey() const {
+			return m_strKey;
+		}
+
+		/**
+		 * @brief 获取键值缩写
+		 */
+		const std::string& GetShortKey() const {
+			return m_strShortKey;
+		}
+
+		/**
+		 * @brief 获取用法描述
+		 */
+		const std::string& GetUsage() const {
+			return m_strUsage;
+		}
+
+	private:
+		/**
+		 * @brief 打印健值
+		 * @param nKeyLen 键值最大长度
+		 * @return 返回执行结果
+		 */
+		std::string _PrintKey(int nKeyLen) const;
+
+	private:
+		const std::string m_strKey;
+		const std::string m_strShortKey;
+		const std::string m_strUsage;
+	};
+
+	/**
+     * @brief 字符串选项
+     */
+	class StringFlag : public FlagBase {
+	public:
+		StringFlag(std::string& strValue, const char* szKey, const char* szShortKey,
+				const char* szUsage, std::string strDefaultValue);
+		virtual ~StringFlag();
+
+	public:
+		/**
+		 * @brief 解析选项
+		 * @param szValue 选项值
+		 * @return 返回执行结果
+		 */
+		virtual bool ParseFlag(const char* szValue);
+
+	private:
+		std::string& m_strValue; ///< 选项值
+	};
+
+	/**
+     * @brief 数值选项
+     */
+	class IntFlag : public FlagBase {
+	public:
+		IntFlag(int& nValue, const char* szKey, const char* szShortKey,
+				const char* szUsage, int nDefaultValue);
+		virtual ~IntFlag();
+
+	public:
+		/**
+		 * @brief 解析选项
+		 * @param szValue 选项值
+		 * @return 返回执行结果
+		 */
+		virtual bool ParseFlag(const char* szValue);
+
+	private:
+		int& m_nValue; ///< 选项值
+	};
+
+	/**
+     * @brief 布尔选项
+     */
+	class BoolFlag : public FlagBase {
+	public:
+		BoolFlag(bool& bValue, const char* szKey, const char* szShortKey,
+				 const char* szUsage, bool bDefaultValue);
+		virtual ~BoolFlag();
+
+	public:
+		/**
+		 * @brief 解析选项
+		 * @param szValue 选项值
+		 * @return 返回执行结果
+		 */
+		virtual bool ParseFlag(const char* szValue);
+
+	private:
+		bool& m_bValue; ///< 选项值
+	};
+
+	/**
+     * @brief 浮点数值选项
+     */
+	class FloatFlag : public FlagBase {
+	public:
+		FloatFlag(float& fValue, const char* szKey, const char* szShortKey,
+				  const char* szUsage, float fDefaultValue);
+		virtual ~FloatFlag();
+
+	public:
+		/**
+		 * @brief 解析选项
+		 * @param szValue 选项值
+		 * @return 返回执行结果
+		 */
+		virtual bool ParseFlag(const char* szValue);
+
+	private:
+		float& m_fValue; ///< 选项值
+	};
+
+	/**
+     * @brief 程序选项
+     */
+	class Flags {
+	public:
+		Flags();
+
+		typedef std::shared_ptr<FlagBase> FlagPtr_t;
+
+	public:
+		/**
+		 * @brief 解析程序选项
+		 * @param argc 参数数
+		 * @param args 参数列表
+		 * @return 返回执行结果
+		 */
+		bool Parse(int argc, const char** args);
+
+		/**
+		 * @brief 打印用法
+		 */
+		void Usage();
+
+		/**
+		 * @brief 添加字符串选项
+		 * @param strValue 绑定变量
+		 * @param szKey 健值
+		 * @param szShortKey 健值缩写
+		 * @param szUsage 用法描述
+		 * @param strDefaultValue 默认值
+		 */
+		void StringValue(std::string& strValue, const char* szKey, const char* szShortKey,
+				const char* szUsage, std::string strDefaultValue = "");
+
+		/**
+		 * @brief 添加数值选项
+		 * @param strValue 绑定变量
+		 * @param szKey 健值
+		 * @param szShortKey 健值缩写
+		 * @param szUsage 用法描述
+		 * @param strDefaultValue 默认值
+		 */
+		void IntValue(int& nValue, const char* szKey, const char* szShortKey,
+				const char* szUsage, int nDefaultValue = 0);
+
+		/**
+		 * @brief 添加布尔选项
+		 * @param strValue 绑定变量
+		 * @param szKey 健值
+		 * @param szShortKey 健值缩写
+		 * @param szUsage 用法描述
+		 * @param strDefaultValue 默认值
+		 */
+		void BoolValue(bool& bValue, const char* szKey, const char* szShortKey,
+				const char* szUsage, bool bDefaultValue = false);
+
+		/**
+		 * @brief 添加浮点数值选项
+		 * @param strValue 绑定变量
+		 * @param szKey 健值
+		 * @param szShortKey 健值缩写
+		 * @param szUsage 用法描述
+		 * @param strDefaultValue 默认值
+		 */
+		void FloatValue(float& fValue, const char* szKey, const char* szShortKey,
+				const char* szUsage, float fDefaultValue = 0.0f);
+
+	private:
+		/**
+		 * @brief 添加选项
+		 * @param szKey 健值
+		 * @param szShortKey 健值缩写
+		 * @param pValue 选项实例
+		 */
+		void _AddValue(const char* szKey, const char* szShortKey, FlagBase* pValue);
+
+	private:
+		std::map<std::string, FlagPtr_t> m_mpValues;
+		std::map<std::string, FlagPtr_t> m_mpKeyValueMap;
+		std::map<std::string, FlagPtr_t> m_mpShortKeyValueMap;
+	};
+}
