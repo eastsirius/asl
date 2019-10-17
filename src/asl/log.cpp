50a7aab06a7a94aa0466add7023439972174a254
cxt <cxt1989422@126.com>
2019-10-17 16:10:44 +0800

    日志模块添加自定义支持

--- src/asl/log.cpp
+++ src/asl/log.cpp
@@ -16,6 +16,8 @@ namespace ASL_NAMESPACE {
     static asl_log_output_t g_outputs[ASL_LOG_MAX_OUTPUT_NUM];
     static int g_module_min_level = 0;
     static Mutex g_loggers_lock;
+    static asl_log_formatter_t g_formatter = NULL;
+    static asl_log_file_namer_t g_file_namer = NULL;
 
     static const char* g_szLogLevelStrings[] = {
             "all",
@@ -43,11 +45,24 @@ namespace ASL_NAMESPACE {
      */
     static void _asl_log_custom_output(asl_log_output_t* output, int level, const char* log);
 
+    /**
+     * @brief 日志格式化函数
+     */
+    static void _asl_log_formatter(char* buf, int size, const asl_log_msg_t* msg, va_list args);
+
+    /**
+     * @brief 日志文件名生成函数
+     */
+    static void _asl_log_file_namer(char* buf, int size);
+
 
     int asl_log_init() {
         g_output_num = 0;
         memset(g_outputs, 0, sizeof(g_outputs));
         g_module_min_level = 0;
+
+        asl_log_set_formatter(NULL);
+        asl_log_set_file_namer(NULL);
 
         asl_log_output_t opt;
         memset(&opt, 0, sizeof(opt));
@@ -75,14 +90,14 @@ namespace ASL_NAMESPACE {
         }
     }
 
-    void asl_log_write(int level, const char* file, int line, const char* fmt, ...) {
+    void asl_log_write(int level, const char* file, const char* func, int line, const char* fmt, ...) {
         va_list args;
         va_start(args, fmt);
-        asl_log_write2(level, file, line, fmt, args);
+        asl_log_write2(level, file, func, line, fmt, args);
         va_end(args);
     }
 
-    void asl_log_write2(int level, const char* file, int line, const char* fmt, va_list args) {
+    void asl_log_write2(int level, const char* file, const char* func, int line, const char* fmt, va_list args) {
         const char* p1, *p2, *p;
 
         if(level < g_module_min_level) {
@@ -95,19 +110,18 @@ namespace ASL_NAMESPACE {
         p = ASL_MAX(p, file);
 
         do {
+            asl_log_msg_t msg = {0};
             char acBuffer[ASL_LOG_MAX_LENGTH];
-            Datetime dt = Datetime::GetLocalTime();
-            int len, i;
+            int i;
 
-            /* 格式化日志 */
-            snprintf(acBuffer, ASL_LOG_MAX_LENGTH - 4, "%2.2d:%2.2d:%2.2d.%3.3d %s: %s(%d): ",
-                    dt.GetHour(), dt.GetMinute(), dt.GetSecond(), dt.GetMillisecond(),
-                    g_szLogLevelStrings[level], p, line);
-            len = (int)strlen(acBuffer);
-            vsnprintf(acBuffer + len, ASL_LOG_MAX_LENGTH - 4 - len, fmt, args);
-            len = (int)strlen(acBuffer);
-            strcpy(acBuffer + len, "\n");
-            len = (int)strlen(acBuffer);
+            msg.timestamp = asl_get_ms_time();
+            msg.level = level;
+            msg.file = p;
+            msg.func = func;
+            msg.line = line;
+            msg.format = fmt;
+            g_formatter(acBuffer, ASL_LOG_MAX_LENGTH - 2, &msg, args);
+            strcat(acBuffer, "\n");
 
             g_loggers_lock.Lock();
             for(i = 0; i < g_output_num; ++i) {
@@ -134,6 +148,14 @@ namespace ASL_NAMESPACE {
         } while(false);
     }
 
+    void asl_log_set_formatter(asl_log_formatter_t formatter) {
+        g_formatter = formatter ? formatter : _asl_log_formatter;
+    }
+
+    void asl_log_set_file_namer(asl_log_file_namer_t namer) {
+        g_file_namer = namer ? namer : _asl_log_file_namer;
+    }
+
 
     static void _asl_log_std_output(asl_log_output_t* output, int level, const char* log) {
         printf("%s", log);
@@ -141,9 +163,11 @@ namespace ASL_NAMESPACE {
 
     static void _asl_log_file_output(asl_log_output_t* output, int level, const char* log) {
         char acPath[1024];
-        Datetime dt = Datetime::GetLocalTime();
 
-        sprintf(acPath, "%s/%4.4d-%2.2d-%2.2d.txt", output->path, dt.GetYear(), dt.GetMonth(), dt.GetDay());
+        strcpy(acPath, output->path);
+        strcat(acPath, "/");
+        int len = strlen(acPath);
+        g_file_namer(acPath + len, sizeof(acPath) - len);
 
         std::ofstream fout(acPath, std::ofstream::app | std::ofstream::binary);
         if(!fout) {
@@ -157,5 +181,18 @@ namespace ASL_NAMESPACE {
         if(output->output_proc) {
             output->output_proc(level, log);
         }
+    }
+
+    static void _asl_log_formatter(char* buf, int size, const asl_log_msg_t* msg, va_list args) {
+        Datetime dt = Datetime::ToLocalTime(Time(msg->timestamp * 1000));
+        snprintf(buf, size - 1, "%2.2d:%2.2d:%2.2d.%3.3d [%s] %s:%d ", dt.GetHour(), dt.GetMinute(),
+                dt.GetSecond(), dt.GetMillisecond(), g_szLogLevelStrings[msg->level], msg->file, msg->line);
+        int len = (int)strlen(buf);
+        vsnprintf(buf + len, size - 1 - len, msg->format, args);
+    }
+
+    static void _asl_log_file_namer(char* buf, int size) {
+        Datetime dt = Datetime::GetLocalTime();
+        snprintf(buf, size, "%4.4d-%2.2d-%2.2d.txt", dt.GetYear(), dt.GetMonth(), dt.GetDay());
     }
 }
--- src/asl/log.hpp
+++ src/asl/log.hpp
@@ -8,6 +8,7 @@
 
 #include "asldef.hpp"
 #include <stdarg.h>
+#include <stdint.h>
 
 /* 日志等级 */
 #define ASL_LOGLEVEL_ALL    0 /*!< 所有 */
@@ -22,7 +23,7 @@
 
 
 #define asl_log_print(level, fmt, ...) \
-    asl_log_write(level, __FILE__, __LINE__, fmt, ##__VA_ARGS__);
+    asl_log_write(level, __FILE__, __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__);
 
 #define asl_log_debug(fmt, ...) \
 	asl_log_print(ASL_LOGLEVEL_DEBUG, fmt, ##__VA_ARGS__)
@@ -57,6 +58,22 @@ namespace ASL_NAMESPACE {
 	 */
 	typedef void (*asl_log_output_proc_t)(int level, const char* log);
 
+    /**
+     * @brief 日志格式化函数
+     * @param buf 输出缓存
+     * @param size 输出缓存大小
+     * @param msg 日志消息
+     * @param args 格式化参数
+     */
+	typedef void (*asl_log_formatter_t)(char* buf, int size, const struct _asl_log_msg_t* msg, va_list args);
+
+    /**
+     * @brief 日志文件名生成函数
+     * @param buf 输出缓存
+     * @param size 输出缓存大小
+     */
+    typedef void (*asl_log_file_namer_t)(char* buf, int size);
+
 	/**
 	 * @brief 日志输出配置
 	 */
@@ -66,6 +83,18 @@ namespace ASL_NAMESPACE {
 		asl_log_output_proc_t output_proc;  /*!< 自定义输出函数(仅自定义输出) */
 		int level;                      	/*!< 日志等级 */
 	} asl_log_output_t;
+
+    /**
+     * @brief 日志消息
+     */
+    typedef struct _asl_log_msg_t {
+        uint64_t timestamp;                 /*!< 日志时间戳 */
+        int level;                      	/*!< 日志等级 */
+        const char* file;                   /*!< 代码文件 */
+        const char* func;                   /*!< 代码所在函数 */
+        int line;                           /*!< 代码所在行 */
+        const char* format;                 /*!< 格式化字符串 */
+    } asl_log_msg_t;
 
 
 	/**
@@ -90,18 +119,32 @@ namespace ASL_NAMESPACE {
 	 * @brief 打印日志
 	 * @param level 日志等级
 	 * @param file 代码文件
+	 * @param func 代码函数
 	 * @param line 代码行
 	 * @param fmt 格式化文本
 	 */
-	void asl_log_write(int level, const char* file, int line, const char* fmt, ...);
+	void asl_log_write(int level, const char* file, const char* func, int line, const char* fmt, ...);
 
 	/**
 	 * @brief 打印日志
 	 * @param level 日志等级
 	 * @param file 代码文件
+	 * @param func 代码函数
 	 * @param line 代码行
 	 * @param fmt 格式化文本
 	 * @param args 参数列表
 	 */
-	void asl_log_write2(int level, const char* file, int line, const char* fmt, va_list args);
+	void asl_log_write2(int level, const char* file, const char* func, int line, const char* fmt, va_list args);
+
+    /**
+     * @brief 设置日志格式化函数
+     * @param formatter 日志格式化函数
+     */
+	void asl_log_set_formatter(asl_log_formatter_t formatter);
+
+    /**
+     * @brief 日志文件名生成函数
+     * @param namer 日志文件名生成函数
+     */
+    void asl_log_set_file_namer(asl_log_file_namer_t namer);
 }
